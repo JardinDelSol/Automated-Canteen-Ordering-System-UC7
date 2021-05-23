979260b37d79580c07943177826120899c1362df
Yujee Song <rswfaf@gmail.com>
2021-05-23 11:13:54 +0900

    db_connection to py file

---
+++ controller.py
@@ -0,0 +1,21 @@
+import db_related.db_connection as connection
+import UI
+
+
+class Controller:
+    def __init__(self, userID):
+        self.userID = userID
+        self.money = connection.MONEY()
+        self.balanceRequest = connection.BalanceSearchRequest(userID)
+        self.reloadRequest = connection.ReloadRequest(userID)
+        self.blanceConnection = connection.BalanceCheckConnection(self.balanceRequest, self.money)
+        self.reloadConnection = connection.ReloadConnection(self.reloadRequest, self.money)
+        self.InterfacePage = UI.UIMaker(UI.root,self.money)
+    
+    def getBalanceRequest(self, userID: str):
+        self.balanceConnection.
+(UserID)
+    def 
+
+        
+
