f17a54ea74c27f5e3cc21e81e627f0fb6f652d2b
HunSeok <gnstjr2000@gmail.com>
2021-05-31 10:02:20 +0900

    test_UI(add invalid input test)

--- test_UI.py
+++ test_UI.py
@@ -7,17 +7,21 @@ Created on Fri May 28 20:39:36 2021
 from controller import *
 
 def test_interface_input():
-    data=["1000","123","0","2000","3000"]
+    data_put=["1000","123","0","2000","3000","12asd",""]
+    data_in=[1000,123,0,2000,3000,0,0]
     
     controller = Controller(userID="a")
     controller.Page.test_flg=2
     controller.Page.start()
-    controller.money
+    
     print(controller.Page.input)
-    for i in data:
+    for i in range(len(data_put)):
         money_before=controller.money.amount
-        controller.Page.test(i)
-        assert controller.Page.input==int(i)
-        money_after=money_before+int(i)
+        
+        controller.Page.test(data_put[i])
+        assert controller.Page.input==data_in[i]
+        
+        money_after=money_before+data_in[i]
+        
         assert controller.money.amount==money_after
         
